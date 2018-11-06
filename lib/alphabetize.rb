41d27423ecd07b141c1372701623335a9aa950ee
Christopher Benedict <chrisjbenedict@gmail.com>
2018-11-06 21:13:02 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -8,8 +8,8 @@ def alphabetize(arr)
   # the second sentence becomes the first sentence in our arr and so on.
   # then sort_by maps the original values and returns a sorted array.
   arr.sort_by do |sentence|
-    # split each sentence in an arr of letters and spaces and 
-    # map a new value from the code block onto each element of the arr 
+    # split each sentence in an arr of letters and spaces and
+    # map a new value from the code block onto each element of the arr
     sentence.split("").map do |character|
       # identifies the index of each character as it occurs in Esperanto
       ESPERANTO_ALPHABET.index(character)
